3db7f8e43eb7b669264bedd6918e950278e1ac26
Shohil Kishore <shohilkishore@wifi-staff-172-24-39-65.net.auckland.ac.nz>
2018-03-01 16:59:26 +1300

    Inital commit

--- trelloData.py
+++ trelloData.py
@@ -6,5 +6,3 @@ student1Data = urlopen("https://trello.com/b/uaCT48b5/chronospatial.json").read(
 
 # Data is in bytes, needs to be decoded so it's JSON serializable.
 student1Decode = student1Data.decode("utf-8")
-
-# Backslashes from the data need to be removed.
